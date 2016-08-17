7a3dd3b3eab64458ea0f3eff3196efb5a89034ed
harishraisinghani <harishraisinghani@gmail.com>
2016-08-17 00:36:00 +0000

    View a user's details

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,4 +11,8 @@ class Admin::UsersController < ApplicationController
     # @user = User.all
     @users = User.order(:firstname).page(params[:page]).per(2)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
