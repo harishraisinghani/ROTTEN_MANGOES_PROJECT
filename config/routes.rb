e4538c1a2c5ac609b48c8535a8a533b2407ac51c
harishraisinghani <harishraisinghani@gmail.com>
2016-08-18 20:22:12 +0000

    Show user profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ RottenMangoes::Application.routes.draw do
 
   get '/search', to: 'movies#index' 
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   #resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
